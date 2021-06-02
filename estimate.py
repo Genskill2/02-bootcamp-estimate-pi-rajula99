546350222558492e16cf4e550d168b20f6ba3bd0
rajula <rajula.abdullah@gmail.com>
2021-06-02 08:27:32 +0530

    fixed errors

--- estimate.py
+++ estimate.py
@@ -28,22 +28,21 @@ class TestMC(unittest.TestCase):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
         
-    
-if __name__ == "__main__":
-
-    def wallis(itn):
+def wallis(itn):
         p = 1
         for i in range(1,itn):
             p *= 4*(i ** 2) / (4 * (i ** 2) - 1) 
         return 2*p
 
-    def monte_carlo(itn):
-        c = 0
-        for i in range(itn):
-            x = 2*random()-1
-            y = 2*random()-1
-            if x*x + y*y <= 1:
-                c += 1
-        return (4.0*c/itn)
+def monte_carlo(itn):
+    c = 0
+    for i in range(itn):
+        x = 2*random()-1
+        y = 2*random()-1
+        if x*x + y*y <= 1:
+            c += 1
+    return (4.0*c/itn) 
+
+if __name__ == "__main__":
 
     unittest.main()
